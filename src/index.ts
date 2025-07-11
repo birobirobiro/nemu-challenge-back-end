01b8588aae2ab222fbc036c9eae63700f3e171f8
João Inácio Neto <birobirobiro.dev@gmail.com>
2025-07-11 12:20:25 -0300

    chore: add dotenv for .env and update server port

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import journeysRouter from "./routes/journeys";
 
 const app = express();
-const port = 3333;
+const port = process.env.PORT || 3333;
 
 app.use("/journeys", journeysRouter);
 
 app.listen(port, () => {
-  console.log("Server is running 🤘");
+  console.log(`Server is running 🤘 on port ${port}`);
 });
